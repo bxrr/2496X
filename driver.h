dd2e91ec981f4ede183aeecc54819cb9d058414a
YKawesome <72176181+YKawesome@users.noreply.github.com>
2022-11-19 10:19:33 -0800

    Update driver.h

--- driver.h
+++ driver.h
@@ -47,7 +47,7 @@ int flywheel_control()
 {
     static int speed_index = 0;
     static bool fly_on = false;
-    std::vector<int> speeds = {345, 375};
+    std::vector<int> speeds = {356, 390};
     if(glb::con.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1))
         fly_on = !fly_on;
 
