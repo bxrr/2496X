7536f433f51cae1109530e677f4193d38601ea7f
Brandon X <68095440+bxrr@users.noreply.github.com>
2023-03-12 19:49:16 -0700

    removed junk

--- global.h
+++ global.h
@@ -21,14 +21,9 @@ namespace glb
     #define P_INTAKE_P 'A'
     #define P_ANGLE_P 'B'
     #define P_EXPANSION_P 'E'
-    #define P_SIDE_EXPAND_P 'F'
-    #define P_COMP_HOOD_P 'G'
     #define P_DISC_SENSOR1 16
-    #define P_DISC_SENSOR2 5
-    #define P_DISC_SENSOR3 19
-
-
     #define P_IMU 13
+
     // objects =============================
     pros::Controller con(pros::E_CONTROLLER_MASTER);
     pros::Motor flywheelL(P_FLY_L, pros::E_MOTOR_GEARSET_06, true);
@@ -36,15 +31,11 @@ namespace glb
     pros::Motor intakeL(P_INTAKE_L, pros::E_MOTOR_GEARSET_06, true);
     pros::Motor intakeR(P_INTAKE_R, pros::E_MOTOR_GEARSET_06, false);
     pros::Distance disc_sensor1(P_DISC_SENSOR1);
-    pros::Distance disc_sensor2(P_DISC_SENSOR2);
-    pros::Distance disc_sensor3(P_DISC_SENSOR3);
     pros::Imu imu(P_IMU);
 
     Piston intakeP(P_INTAKE_P);
-    Piston hoodP(P_COMP_HOOD_P);
     Piston angleP(P_ANGLE_P, false, true);
     Piston expansionP(P_EXPANSION_P);
-    Piston sideExpandP(P_SIDE_EXPAND_P);
     Chassis chas({P_BL, -P_FL}, {P_BR, -P_FR}, pros::E_MOTOR_GEARSET_18, false);
     Auton *auton;
 }
